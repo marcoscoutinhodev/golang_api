0042d9cd4d36b06523a11df8c0e409ea2d5e5500
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2022-06-04 13:52:38 -0300

    feat: add route to get task by id

--- api/routes.go
+++ api/routes.go
@@ -9,7 +9,8 @@ func Routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/task/add", controller.AddTaskController)
-	mux.HandleFunc("/task/get", controller.GetTaskController)
+	mux.HandleFunc("/task/get", controller.GetTasksController)
+	mux.HandleFunc("/task/get-by-id", controller.GetTaskByIdController)
 	mux.HandleFunc("/task/update", controller.UpdateTaskController)
 	mux.HandleFunc("/task/delete", controller.DeleteTaskController)
 	mux.HandleFunc("/task/toggle-done-field", controller.ToggleDoneFieldTaskController)
