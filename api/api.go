8f0a4bc1cc673def032cfae5ccad065a1f5c3a11
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2022-06-04 12:13:01 -0300

    feat: add helper to create DB & Table on server start

--- api/api.go
+++ api/api.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	database "golang_api/api/database/sql"
 )
 
 func Server() {
@@ -12,8 +14,37 @@ func Server() {
 		Handler: Routes(),
 	}
 
+	databaseHelperDev()
+	fmt.Println("tbTask was created in mysql instance")
+
 	fmt.Println("Starting sever...")
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func databaseHelperDev() {
+	db, err := database.Connection()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		log.Fatal("MYSQL connection error")
+	}
+
+	defer db.Close()
+
+	db.Exec("CREATE DATABASE IF NOT EXISTS golang_db")
+	db.Exec("USE golang_db")
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS tbTask (
+		id 					INT AUTO_INCREMENT,
+		title				VARCHAR(40) NOT NULL,
+		description VARCHAR(150) NOT NULL,
+		done 				TINYINT NOT NULL,
+
+		PRIMARY KEY (id)
+	)`)
+
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 }
